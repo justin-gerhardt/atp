0b3e072d48232cc81b362a059d1b3cf3955229fb
Justin Gerhardt <justin@gerhardt.link>
2018-11-07 00:59:49 -0500

    start download launching

--- src/main.go
+++ src/main.go
@@ -9,43 +9,123 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecs"
+
+	"github.com/pkg/errors"
 )
 
-type Response events.APIGatewayProxyResponse
-
-func Handler(ctx context.Context) (Response, error) {
-	if isLive() {
-
+func isDownloaderTaskAlreadyRunning(ecsService *ecs.ECS) (bool, error) {
+	var nextToken *string
+	for {
+		list, err := ecsService.ListTasks(&ecs.ListTasksInput{
+			Cluster:       aws.String(os.Getenv("CLUSTER")),
+			DesiredStatus: aws.String("RUNNING"),
+			NextToken:     nextToken,
+		})
+		if err != nil {
+			return false, errors.Wrap(err, "Failed to list tasks on atp cluster")
+		}
+		nextToken = list.NextToken
+		if len(list.TaskArns) == 0 {
+			break
+		}
+		tasks, err := ecsService.DescribeTasks(&ecs.DescribeTasksInput{
+			Cluster: aws.String(os.Getenv("CLUSTER")),
+			Tasks:   list.TaskArns,
+		})
+		if err != nil {
+			return false, errors.Wrap(err, "Failed to describe running tasks")
+		}
+		for _, task := range tasks.Tasks {
+			if *task.TaskDefinitionArn == os.Getenv("TASK_DEFINITION") {
+				log.Println("Downloader task is already running")
+				return true, nil
+			}
+		}
+		if nextToken == nil {
+			break
+		}
 	}
-	return Response{Body: "aaaaaaa", StatusCode: 200}, nil
+	log.Println("Downloader task not already running")
+	return false, nil
+}
+
+func launchDownloaderTask() error {
+	sess, err := session.NewSession()
+	if err != nil {
+		return errors.Wrap(err, "Failed to create aws session")
+	}
+	ecsService := ecs.New(sess)
+	isAlreadyRunning, err := isDownloaderTaskAlreadyRunning(ecsService)
+	if err != nil {
+		return errors.Wrap(err, "Failed to check if download task is already running")
+	}
+	if !isAlreadyRunning {
+		_, err := ecsService.RunTask(&ecs.RunTaskInput{
+			Cluster:    aws.String(os.Getenv("CLUSTER")),
+			Count:      aws.Int64(1),
+			LaunchType: aws.String(ecs.LaunchTypeFargate),
+			NetworkConfiguration: &ecs.NetworkConfiguration{
+				AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
+					AssignPublicIp: aws.String(ecs.AssignPublicIpEnabled),
+					Subnets:        []*string{aws.String(os.Getenv("SUBNET"))},
+					SecurityGroups: []*string{aws.String(os.Getenv("SECURITY_GROUP"))},
+				},
+			},
+			TaskDefinition: aws.String(os.Getenv("TASK_DEFINITION")),
+		})
+		if err != nil {
+			return errors.Wrap(err, "Failed to start task")
+		}
+	}
+	return nil
+}
+
+type response events.APIGatewayProxyResponse
+
+func handler(ctx context.Context) (response, error) {
+	live, err := isLive()
+	if err != nil {
+		log.Printf("%s", err)
+		return response{}, err
+	}
+	if live {
+		err := launchDownloaderTask()
+		if err != nil {
+			log.Printf("%s", err)
+			return response{}, err
+		}
+	}
+	return response{Body: "aaaaaaa", StatusCode: 200}, nil
 
 }
 
-func isLive() bool {
+func isLive() (bool, error) {
 	stream, err := http.Get("http://marco.org:8001/listen")
 	//stream, err := http.Get("https://httpstat.us/401")
 	if err != nil {
-		log.Fatal("The request to marco.org failed")
-		return false
+		return false, errors.Wrap(err, "Can't check if stream is online. The request failed")
 	}
 	defer stream.Body.Close()
-
-	if stream.StatusCode != http.StatusOK {
-		if stream.StatusCode == http.StatusNotFound {
-			log.Println("The stream is not active")
-			return false
-		}
-		log.Fatal("The stream returned a unexpected code: " + strconv.Itoa(stream.StatusCode))
-		return false
+	switch stream.StatusCode {
+	case http.StatusNotFound:
+		log.Println("The stream is not active")
+		return false, nil
+	case http.StatusOK:
+		log.Println("The stream is live")
+		return true, nil
+	default:
+		return false, errors.New("The stream returned a unexpected code: " + strconv.Itoa(stream.StatusCode))
 	}
-	log.Println("The stream is live")
-	return true
 }
 
 func main() {
 	if os.Getenv("IS_OFFLINE") == "TRUE" {
-		Handler(nil)
+	    handler(nil)
 		return
 	}
-	lambda.Start(Handler)
+	lambda.Start(handler)
 }
